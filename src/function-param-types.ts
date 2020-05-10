f9956cd0a068d3fdeaa18e5def400bf6c6d9b4f0
Alex Trew <trew13690@gmail.com>
2020-05-09 22:05:07 -0700

    function Param Types

---
+++ src/function-param-types.ts
@@ -0,0 +1,16 @@
+
+
+function getPerson(firstName: string, middleName: string, lastName: string): string{
+
+    return `${firstName} + ${middleName} ${lastName}`;
+
+}
+
+getPerson('Alex', 'Sebastien', 'Trew')
+
+
+function returnNothing(): void{
+
+}
+
+const val = returnNothing();
