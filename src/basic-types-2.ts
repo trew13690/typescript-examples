a35b505723447885e1d8e51e2227a44a4b5bc7d7
Alex Trew <trew13690@gmail.com>
2020-05-09 21:59:20 -0700

    Basic types 2

---
+++ src/basic-types-2.ts
@@ -0,0 +1,14 @@
+const fName: string = 'Alex Trew'
+const age: number = 27
+
+const birthDate: Date = new Date(1992, 2, 13)
+const isActive: boolean = true
+
+const obj: any = {}
+
+const ages: number[] = [23, 4,32,12,133]
+const names: string[] = ['Alex', 'Mai', 'Han']
+
+const agesAgain: Array<number> = [23, 444,223,1112,34]
+
+const pairs: [string, number] = ['Alex', 28]
